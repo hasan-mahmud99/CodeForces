dbd7b352058bf49e603c70b94639efc534303914
Hasan Mahmud <129969671+hasan-mahmud99@users.noreply.github.com>
2023-09-26 00:37:03 +0600

    Create 467A-George_accom.cpp

---
+++ 467A-George_accom.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main() {
+
+	int n,sum1=0,sum2=0,c=0;
+	cin>>n;
+	int p,q;
+	while(n--){
+		cin>>p;
+		cin>>q;
+		if(q-p>1) c++;
+
+	}
+     cout<<c;
+
+
+}
