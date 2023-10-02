959a2e0cf91dc8935995d0c172f1a6e51224faa2
Hasan Mahmud <129969671+hasan-mahmud99@users.noreply.github.com>
2023-10-02 15:54:32 +0600

    Create 581A-Vasya the Hipstar.cpp

---
+++ 581A-Vasya the Hipstar.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+   int a,b,d,c;
+   cin>>a>>b;
+   if(a>b) {
+     d=b;
+     cout<<d<<" ";
+     c=(a-b)/2;
+     cout<<c;
+   }
+        
+
+   else {
+     d=a;
+     cout<<d<<" ";
+     c=(b-a)/2;
+     cout<<c;
+  
+}
+   }
