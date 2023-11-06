f8504f223e97a1678f48337a40e1816b1160b4a9
Hasan Mahmud <129969671+hasan-mahmud99@users.noreply.github.com>
2023-11-06 11:02:01 +0600

    Create 1870A-Plus or Minus.cpp

---
+++ 1870A-Plus or Minus.cpp
@@ -0,0 +1,18 @@
+//Hasan
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+  int a,b,c;
+  cin>>a>>b>>c;
+  if(a+b==c) cout<<"+"<<endl;
+  else cout<<"-"<<endl;
+    
+  }
+  
+}
+
+
