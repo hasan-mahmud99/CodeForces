adc309aa6a74e0da33a3dd7b75aff3d90a6c1497
Hasan Mahmud <129969671+hasan-mahmud99@users.noreply.github.com>
2023-11-10 00:00:50 +0600

    Create 1722A-Spell Check.cpp

---
+++ 1722A-Spell Check.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+
+{      int t;
+       cin>>t;
+       while(t--){
+       string s;
+       int n;
+       cin>>n;
+       cin>>s;
+       sort(s.begin(),s.end());
+       if(s=="Timru"){
+              cout<<"YES"<<endl;
+       }
+       else cout<<"NO"<<endl;
+      
+}
+}
