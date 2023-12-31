7e7740e815e1c29da3e88fa99c6ce0324ce9608e
Hasan Mahmud <129969671+hasan-mahmud99@users.noreply.github.com>
2023-12-31 19:06:22 +0600

    Create 451A-Game with sticks.cpp

---
+++ 451A-Game with sticks.cpp
@@ -0,0 +1,13 @@
+//Hasan
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    if(min(n,m)%2==0) cout<<"Malvika";
+    else cout<<"Akshat";
+    return 0;
+}
