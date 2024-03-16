c20de3754cf9297523b030c9dd63ece46e656b6c
Hasan Mahmud <129969671+hasan-mahmud99@users.noreply.github.com>
2024-03-16 19:40:49 +0600

    Create 1941C - Rudolf and the Ugly String.cpp

---
+++ 1941C - Rudolf and the Ugly String.cpp
@@ -0,0 +1,29 @@
+//Hasan
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+        int c=0,n;
+        cin>>n;
+       string s;
+       cin>>s;
+       for(int i=0;i<s.size();i++){
+        if(s[i]=='p' && s[i+1]=='i' && s[i+2]=='e'){
+            i+=2;
+            c++;
+        }
+        else if(s[i]=='m' && s[i+1]=='a' && s[i+2]=='p'){
+            i+=2;
+            c++;
+        }
+       }
+        cout<<c<<endl;
+       
+    }
+    return 0;
+}
