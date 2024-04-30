7562d578a1cfdfeef48a7b16523111344e96acdf
Hasan Mahmud <129969671+hasan-mahmud99@users.noreply.github.com>
2024-04-30 19:36:17 +0600

    Create 1772A - A+B?.cpp

---
+++ 1772A - A+B?.cpp
@@ -0,0 +1,18 @@
+//Hasan
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+
+
+int main()
+{   
+   ll t,a,b;
+   char c;
+   cin>>t;
+   while(t--){
+    cin>>a>>c>>b;
+    cout<<a+b<<endl;
+
+}
+}
