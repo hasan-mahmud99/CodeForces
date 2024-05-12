f041042e5a3600500ab663e10ee9e971a71fc094
Hasan Mahmud <129969671+hasan-mahmud99@users.noreply.github.com>
2024-05-12 10:37:37 +0600

    Create 630C - Lucky Numbers.cpp

---
+++ 630C - Lucky Numbers.cpp
@@ -0,0 +1,14 @@
+//Hasan
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    
+        ll n;
+        cin>>n;
+        cout << (1ll << (n+1)) - 2;
+    
+    return 0;
+}
