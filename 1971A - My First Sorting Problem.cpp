5fabe06f2945e258d8d1b1a6e6763de59272cfea
Hasan Mahmud <129969671+hasan-mahmud99@users.noreply.github.com>
2024-05-12 10:39:40 +0600

    Create 1971A - My First Sorting Problem.cpp

---
+++ 1971A - My First Sorting Problem.cpp
@@ -0,0 +1,18 @@
+//Hasan
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+
+
+int main()
+{   
+   ll t,a,b;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b;
+        cout<<min(a,b)<<" "<<max(a,b)<<endl;
+        
+}
+}
